f9dd197fb4d520b08d1014faedcbc0147c62e6a5
blerner5 <blerner5@student.touro.edu>
2026-03-19 19:44:01 -0400

    i added to the 3 files in backend

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,12 @@
-# app.py
-from flask import Flask
+from backend import create_app
 
-app = Flask(__name__)
+app = create_app()
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "Hello, World!"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
